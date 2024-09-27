a44f5ee878ec42a77bd2a59dbdaea684349320e4
akbw1998 <akbw1998@gmail.com>
2024-09-26 22:27:05 -0400

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,7 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js"; 
-
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary(){
   updateCheckoutQuantity()
@@ -79,6 +79,7 @@ export function renderOrderSummary(){
           removeFromCart(productId)
           document.querySelector(`.js-cart-item-container-${productId}`).remove()
           updateCheckoutQuantity()
+          renderPaymentSummary()
       })
   })
 
@@ -122,6 +123,7 @@ export function renderOrderSummary(){
             const {productId, deliveryOptionId} = deliveryOption.dataset
             updateDeliveryOption(productId, deliveryOptionId)
             renderOrderSummary()
+            renderPaymentSummary()
           })
         })
 }
